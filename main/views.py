9835c180baad437afd80c42beea0e514e6abc3d7
syrfmy <syariefmulyadi24@gmail.com>
2023-09-06 15:51:26 +0700

    context fix

--- main/views.py
+++ main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP A'
+        'name': 'Muh. Syarief Mulyadi',
+        'class': 'PBP E'
     }
 
     return render(request, "main.html", context)
